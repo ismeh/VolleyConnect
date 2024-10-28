ae9b17f804971f5ecbd5c66cdea7d0d37fbd6c42
ismeh <gadeismael@hotmail.com>
2024-10-28 10:51:33 +0100

    add: test de ejemplo

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src } = require('gulp');
+const jest = require('gulp-jest').default;
+
+var paths = {
+    public: {
+      src: 'src/public/*',
+    },
+    views: {
+      src: 'src/views/*',
+    }
+  };
+
+function test() {
+    return src('test').pipe(jest({
+      preprocessorIgnorePatterns: ['/node_modules/'],
+      automock: false
+    }));
+  }
+
+exports.test = test;
+
+
