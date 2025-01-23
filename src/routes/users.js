99a1e64d32955dbedcd4e0b944ef94ac03bbe84d
ismeh <gadeismael@hotmail.com>
2025-01-23 01:49:28 +0100

    :sparkles: add delete request for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,5 +7,6 @@ usersRouter.get('/', UserController.getAll);
 usersRouter.get('/:id', UserController.getById);
 usersRouter.post('/', UserController.create);
 usersRouter.patch('/:id', UserController.update);
+usersRouter.delete('/:id', UserController.delete);
 
 module.exports = usersRouter;
